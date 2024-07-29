d9017203f836a04935da5fbe9a793e4b233f0fb8
root <1241062101@qq.com>
2024-07-29 11:24:05 +0800

    feat(content-api): add course category tree node query endpoint
    - Introduce CourseCategoryService injection in CourseCategoryController.- Implement queryTreeNodes method in CourseCategoryService to retrieve tree nodes.
    - Modify queryTreeNodes endpoint to call the new service method.
    
    The changes allow the API to provide course category tree nodes upon request,
    enhancing the content API's functionality by offering a structured way to access
    course categories.

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseCategoryController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseCategoryController.java
@@ -1,7 +1,9 @@
 package com.xuecheng.content.api;
 
 import com.xuecheng.content.model.dto.CourseCategoryTreeDto;
+import com.xuecheng.content.service.CourseCategoryService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +12,12 @@ import java.util.List;
 @Slf4j
 @RestController
 public class CourseCategoryController {
+
+  @Autowired
+  CourseCategoryService courseCategoryService;
+
   @GetMapping("/course-category/tree-nodes")
   public List<CourseCategoryTreeDto> queryTreeNodes() {
-    return null;
+    return courseCategoryService.queryTreeNodes("1");
   }
 }
